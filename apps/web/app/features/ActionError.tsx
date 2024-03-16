7d03136a3d6549d70922841a81cd788e9df7a3a9
Matheus Ale <m.matheus.ale@gmail.com>
2024-03-16 18:34:04 -0300

    chore: delete some files

--- apps/web/app/features/ActionError.tsx
+++ apps/web/app/features/ActionError.tsx
@@ -1,6 +1,21 @@
-import { getErrorMessage } from "@/api/getErrorMessage";
 import { useRouteError } from "@remix-run/react";
 import clsx from "clsx";
+
+import { isRouteErrorResponse } from "@remix-run/react";
+
+export const getErrorMessage = (error: unknown) => {
+  if (!error) return null;
+
+  if (isRouteErrorResponse(error)) {
+    return `${error.status} ${error.statusText}`;
+  } else if (error instanceof Error) {
+    const msg = error.message;
+    const [message] = msg.split(":");
+    return message;
+  } else {
+    return "Unknown Error";
+  }
+};
 
 export const ActionError = ({
   className,
